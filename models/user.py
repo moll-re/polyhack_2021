d8c7efe7e033edf4f45ecebd551b507a01d5c6b2
Remy Moll <remoll@ethz.ch>
2021-10-31 00:56:14 +0200

    better profile page

--- models/user.py
+++ models/user.py
@@ -1,4 +1,5 @@
 class User:
+    id = 0
     name = ""
     event_preferences = []
     event_blacklist = []
@@ -6,12 +7,36 @@ class User:
     group_size = 0
     min_age = 0
     max_age = 0
+    travel_history = [] # (Name, coordinates)
+    co2_savings = []
     
     def __init__(self, **kwargs):
+        self.id = kwargs.pop("id")
         self.name = kwargs.pop("name")
         self.home_coordinates = kwargs.pop("home_coordinates")
         self.event_preferences = kwargs.pop("event_preferences")
-        self.event_blacklist = kwargs.pop("name")
+        self.event_blacklist = kwargs.pop("event_blacklist")
         self.group_size = kwargs.pop("group_size")
         self.min_age = kwargs.pop("min_age")
         self.max_age = kwargs.pop("max_age")
+
+
+    @property
+    def number_of_trips(self):
+        return len(self.travel_history)
+    
+    @property
+    def total_co2_savings(self):
+        return sum(self.co2_savings)
+
+    
+class Users:
+    _users = []
+
+    def add_user(self, **kwargs):
+        self._users.append(User(**kwargs))
+
+    def get_by_id(self, id):
+        for u in self._users:
+            if u.id == id:
+                return u
--- server.py
+++ server.py
@@ -1,37 +1,40 @@
-from flask import Flask, render_template, request, jsonify, make_response, request
+from flask import Flask, render_template, request, request, session
 import random
-import time
 
-from flask.templating import render_template_string
+from apis.interactive_maps import SwissMap
+from models.user import Users
+
+USERBASE = Users()
+USERBASE.add_user(id=239842123, name="Remy", event_preferences=["hiking","skiing"], event_blacklist = [], home_coordinates=[0,0], group_size=1, min_age=20, max_age=20)
+MAP = SwissMap()
 
 app = Flask(__name__)
-
-heading = "Lorem ipsum dolor sit amet."
-
-content = """
-Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-Repellat inventore assumenda laboriosam, 
-obcaecati saepe pariatur atque est? Quam, molestias nisi.
-"""
-
-db = list()  # The mock database
-
-posts = 500  # num posts to generate
-
-quantity = 20  # num posts to return per request
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
 
 @app.route("/")
 def index():
-    """ Route to render the HTML """
+    session["user_id"] = 239842123 # a perfectly safe login, hem hem
     context = {
         "title" : f"Event title {id}",
         "image_name" : "fallback.jpg",
         "star_rating" : random.randint(1,4),
         "reviews" : random.randint(10,30),
-        "user" : "Remy"
+        "user" : USERBASE.get_by_id(session["user_id"]).name
     }
-    return render_template("base.html", context=context)
+    return render_template("event_overview.html", context=context)
+
+
+
+@app.route("/profile")
+def profile():
+    uid = session["user_id"]
+    user = USERBASE.get_by_id(uid)
+
+    context = {
+        "map_path" : MAP.travel_history_map(user.id, user.travel_history)
+    }
+    return render_template("user_detail.html", conbtext=context, user=user)
 
 
 
@@ -46,6 +49,7 @@ def get_event():
         "user" : "Remy"
     }
     return render_template("event_card.html", context=context)
+
 
 
 app.run(port=8000, debug=True)
