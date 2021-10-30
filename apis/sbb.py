b8c241b1251e07171b635ef4d23b0442d9c54b5f
Miupro <micheldatz@outlook.de>
2021-10-31 00:33:42 +0200

    cleanup

--- apis/sbb.py
+++ apis/sbb.py
@@ -27,28 +27,3 @@ class SBBWrapper:
         }
         return auth
 
-
-        
-"""
-sbb=SBBWrapper()
-print(sbb.wrapper.get('name':'Bern'))
-"""
-
-"""
-header_dict = {'api_key' : '814d86a3ffc49c350bb7dcc48cac69b3'}
-
-response =  requests.get('https://journey-maps.api.sbb.ch:443', headers=header_dict)
-print(response.text)
-"""
-"""
-header_dict = {'api_key' : 'xxxxx'}
-response = requests.get('https://journey-pois-int.api.sbb.ch/', headers=header_dict)
-print(response)
-"""
-"""
-header_dict = {'key':'yourtest-outdoora-ctiveapi',
-'project' : 'api-dev-oa' }
-response = requests.get('http://www.outdooractive.com/api', headers=header_dict).json()
-print(response)
-print('finish')
-"""
